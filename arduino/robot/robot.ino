b54a9f9c0122c969d2a7968c20f3489c2f851320
Anthony Gallagher <anthonycgallagher@gmail.com>
2017-01-24 15:39:15 +0000

    Added connection test code to robot.ino

--- arduino/robot/robot.ino
+++ arduino/robot/robot.ino
@@ -1,1 +1,16 @@
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Hello, World!"));
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String msg = Serial.readString();
+    Serial.println(msg);
+    if (msg == "ping") {
+      Serial.println(F("Pong!"));
+    }      
+  }
+}
+
 
