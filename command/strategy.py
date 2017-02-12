5545a163c93a6b5cd72a018422a741a56dda9bfb
Filipe Wang <s1409854@haefliger.inf.ed.ac.uk>
2017-02-12 16:35:51 +0000

    Added initial strategy, scrapped old one

--- command/strategy.py
+++ command/strategy.py
@@ -19,6 +19,13 @@ LEFTGOALX = 21
 LEFTGOALY = 109
 RIGHTGOALX = 286
 RIGHTGOALY = 111
+CORNER14X = 50
+CORNER14Y = 108
+CORNER23X = 250
+CORNER23Y = 108
+MIDX = 160
+MIDY 108
+
 
 class Strategy(object):
     def __init__(self):
@@ -39,7 +46,7 @@ class Strategy(object):
                 robot2 = curr_world.robots[2]
                 robot3 = curr_world.robots[3]
                 if robot0 != None:
-                    print "Robot0: ", robot0.x , "  " , robot0.y
+                    print "Robot0: ", robot0.x , "  " , robot0.y, " ", robot0.rot
                 if robot1 != None:
                     print "Robot1: ", robot1.x , "  " , robot1.y
                 if robot2 != None:
@@ -120,112 +127,64 @@ class Strategy(object):
         Coms.stop()
 
     @staticmethod
-    def start():
-        #Coms.turn(get_angle_to_send(int(value))) TODO: Turn like this
+    def start(corner,start_x,start_y):
         Coms.start_comunications()
-
-        # Get world model and some values
-        curr_world = waitForWorld(False , requireBall = True , no_oponents = 0)
-        ball = curr_world.ball
-        robots = curr_world.robots
-        robot1 = curr_world.robots[0]
-        robot2 = curr_world.robots[1]
-        robot3 = curr_world.robots[2]
-        robot4 = curr_world.robots[3]
-
-
-        if robot1 != None and ball != None:
-            list = []
-            calculated_time = time.time() + 1
-            while(calculated_time > time.time()):
-                temp_world = World.get_world()
-                list.append(temp_world.robots[0].rot)
-            ball_zone = ball.get_zone()
-            side = World.our_side
-            print "Ball: ", ball.x, "  ", ball.y
-            robot1.rot = np.median(list)
-            angle_to_ball = us_to_obj_angle(robot1,ball)
-            print "Angle to ball: " , angle_to_ball
-            #print "Math god angle: " , mathgod(robot1.x,robot1.y,robot1.rot,ball.x,ball.y)
-            goal_center = World.get_goal_center(False)
-            C = namedtuple("C" , "x y")
-            #print "Angle to goal: ", angle_to_goal
-
-            oldx = robot1.x
-            oldy = robot1.y
-            print "Side currently on: ", side, " Zone: ", ball_zone, " Robot x: ", robot1.x
-            if side == "Left":
-                if (ball_zone == 0 and (robot1.x > ball.x)):
-                    print "LEFT DEFENSE"
+        starting_strategy = True
+        while True:
+            if starting_strategy:
+                starting_strategy = False
+                if corner == 1 or corner == 4:
                     C = namedtuple("C" , "x y")
-                    angle_to_corner = us_to_obj_angle(robot1,C(TOPLEFX,TOPLEFY))
+                    robot_temp = World.Robot(start_x,start_y,0,0,0,0)
+                    time_to_object = get_time_to_travel(robot_temp.x,CORNER14X,robot_temp.y,CORNER14Y)
+                    angle_to_obj = us_to_obj_angle(robot_temp,C(CORNER14X,CORNER14Y))
+                    Coms.turn(get_angle_to_send(int(angle_to_obj)))
+                    time.sleep(1.5)
+                    Coms.reverse(200)
+                    time.sleep(time_to_object)
                     Coms.stop()
-                    time.sleep(0.3)
-                    Coms.stop()
-                    time.sleep(0.3)
-                    Coms.stop()
-                    time.sleep(0.3)
-                    Coms.turn(-angle_to_corner)
-                    time.sleep(1)
-                    Coms.go()
-                    time.sleep(3.3)
-                    Coms.stop()
-                    time.sleep(0.5)
-                    Coms.reverse(1)
-                    time.sleep(0.7)
-                    Coms.stop()
-                else:
-                    if math.fabs(angle_to_ball) < 10:
-                        Coms.go()
+                    time_to_mid = get_time_to_travel(CORNER14X,MIDX,CORNER14Y,MIDY)
+                    if corner == 1:
+                        Coms.turn(get_angle_to_send(int(270)))
                         time.sleep(1.5)
+                        Coms.reverse(200)
+                        time.sleep(time_to_mid)
                         Coms.stop()
                     else:
+                        Coms.turn(get_angle_to_send(int(90)))
+                        time.sleep(1.5)
+                        Coms.reverse(200)
+                        time.sleep(time_to_mid)
                         Coms.stop()
-                        Coms.turn(-angle_to_ball)
-                        time.sleep(1)
-                        Coms.go()
-                        time.sleep(0.6)
+                else:
+                    C = namedtuple("C" , "x y")
+                    time_to_object = get_time_to_travel(robot_temp.x,CORNER23X,robot_temp.y,CORNER23Y)
+                    angle_to_obj = us_to_obj_angle(robot_temp,C(CORNER23X,CORNER23Y))
+                    Coms.turn(get_angle_to_send(int(angle_to_obj)))
+                    time.sleep(1.5)
+                    Coms.reverse(200)
+                    time.sleep(time_to_object)
+                    Coms.stop()
+                    time_to_mid = get_time_to_travel(CORNER23X,MIDX,CORNER23Y,MIDY)
+                    if corner == 2:
+                        Coms.turn(get_angle_to_send(int(90)))
+                        time.sleep(1.5)
+                        Coms.reverse(200)
+                        time.sleep(time_to_mid)
+                        Coms.stop()
+                    else:
+                        Coms.turn(get_angle_to_send(int(270)))
+                        time.sleep(1.5)
+                        Coms.reverse(200)
+                        time.sleep(time_to_mid)
                         Coms.stop()
             else:
-                if (ball_zone == 0 and (robot1.x < ball.x)):
-                    print side
-                    print "RIGHT DEFENSE"
-                    C = namedtuple("C" , "x y")
-                    angle_to_corner = us_to_obj_angle(robot1,C(TOPRIGX,TOPRIGY))
-                    Coms.stop()
-                    time.sleep(0.3)
-                    Coms.stop()
-                    time.sleep(0.3)
-                    Coms.stop()
-                    time.sleep(0.3)
-                    Coms.turn(-angle_to_corner)
-                    time.sleep(1)
-                    Coms.go()
-                    time.sleep(3.3)
-                    Coms.stop()
-                    time.sleep(0.5)
-                    Coms.reverse(1)
-                    time.sleep(0.7)
-                    Coms.stop()
-                else:
-                    if math.fabs(angle_to_ball) < 10:
-                        Coms.go()
-                        time.sleep(1.5)
-                        Coms.stop()
-                    else:
-                        Coms.stop()
-                        Coms.turn(-angle_to_ball)
-                        time.sleep(1)
-                        Coms.go()
-                        time.sleep(0.6)
-                        Coms.stop()
-        if ball_close(robot1,ball):
-            Coms.turn(360)
-            time.sleep(0.6)
-            Coms.stop()
-        final_world = World.get_world()
-        future_robot = final_world.robots[1]
-        if (math.fabs(oldx-future_robot.x) + math.fabs(oldy - future_robot.y)) < 7:
-            Coms.reverse(1)
-            time.sleep(1)
-            Coms.stop()
+                # Normal strategy here
+                curr_world = World.get_world()
+
+                ball = curr_world.ball
+                robots = curr_world.robots
+                robot0 = curr_world.robots[0]
+                robot1 = curr_world.robots[1]
+                robot2 = curr_world.robots[2]
+                robot3 = curr_world.robots[3]
