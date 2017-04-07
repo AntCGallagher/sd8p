82dc7d19b7dcd115232f97b02ef0b02914de53a8
zachbpd <andrazaharia@ymail.com>
2017-04-07 13:24:29 +0100

    delay for grabber

--- arduino/robot/GoInstruction.cpp
+++ arduino/robot/GoInstruction.cpp
@@ -38,6 +38,7 @@ bool GoInstruction::progress() {
   if (IRSensor.getDistanceCentimeter() < 8) {
     Serial.println(F("$BALL;"));
     greenMotorMove(GRABBER_IDX, 70, MOTOR_BWD);
+    delay(100);
     greenMotorMove(LH_IDX, 0, MOTOR_BRAKE);
     greenMotorMove(RH_IDX, 0, MOTOR_BRAKE);
     return true;
